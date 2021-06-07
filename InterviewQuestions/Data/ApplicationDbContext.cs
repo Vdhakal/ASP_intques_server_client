e5afa749c46299def2bae1cecf0ced3d43261726
Vaskar Dhakal <vdhakal10@gmail.com>
2021-06-06 22:19:21 -0500

    third

--- InterviewQuestions/Data/ApplicationDbContext.cs
+++ InterviewQuestions/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using InterviewQuestions.Models;
 
 namespace InterviewQuestions.Data
 {
@@ -12,5 +13,6 @@ namespace InterviewQuestions.Data
             : base(options)
         {
         }
+        public DbSet<InterviewQuestions.Models.Interview> Interview { get; set; }
     }
 }
---
+++ InterviewQuestions/Models/Interview.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterviewQuestions.Models
+{
+    public class Interview
+    {
+        public int Id { get; set; }
+        public string InterviewQuestion { get; set; }
+        public string InterviewAnswer { get; set; }
+        
+        public Interview() 
+        { 
+            
+        }
+    }
+}
