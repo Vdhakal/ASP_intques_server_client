4a826cf4be3b3420550f4c1fd84da5203d8188f3
Vaskar Dhakal <vdhakal10@gmail.com>
2021-06-06 22:34:35 -0500

    fourth

---
+++ InterviewQuestions/Data/Migrations/20210607032136_initialsetup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InterviewQuestions.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Interview",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    InterviewQuestion = table.Column<string>(nullable: true),
+                    InterviewAnswer = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Interview", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Interview");
+        }
+    }
+}
